7513d030fdebeb0f0b4558fd3b7d6ddbcf2cb14e
Kalilou Diaby <kalilou.diaby@sinch.com>
2018-04-10 17:25:19 +0200

    Some work done

--- Vagrantfile
+++ Vagrantfile
@@ -10,13 +10,15 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
       search.vm.hostname = "elk-server"
       search.vm.network "private_network", ip: "192.168.50.200"
       search.vm.synced_folder ".", "/home/vagrant/sync", disabled: true
+      config.vm.synced_folder "synced_folder", "/home/vagrant/synced_folder", type: "nfs"
       search.ssh.forward_agent = true
       search.ssh.insert_key = false
       search.ssh.private_key_path = ["~/.vagrant.d/insecure_private_key" ] 
 
       config.vm.provider "virtualbox" do |v|
-          v.memory = 8024
+          v.memory = 4024
           v.cpus = 2
+          v.name = "elk"
       end
     end
 
--- search_guard.sh
+++ search_guard.sh
@@ -17,8 +17,6 @@
 
 # sudo yum -y install elasticsearch
 
-# sudo systemctl restart elasticsearch
-# sudo systemctl enable elasticsearch
 
 
 # echo '[kibana-6.x]
@@ -33,13 +31,45 @@
 
 # sudo yum -y install kibana
 
-# sudo systemctl restart kibana
-# sudo systemctl enable kibana
+# echo '[logstash-6.x]
+# name=Elastic repository for 6.x packages
+# baseurl=https://artifacts.elastic.co/packages/6.x/yum
+# gpgcheck=1
+# gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+# enabled=1
+# autorefresh=1
+# type=rpm-md
+# ' | sudo tee /etc/yum.repos.d/logstash.repo
+
+# sudo yum -y install logstash
 
 # sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install -b com.floragunn:search-guard-6:6.2.3-22.0
 
-#sudo /usr/share/kibana/bin/kibana-plugin install search-guard-kibana-plugin:6.2.3-11
 
-cd /root && wget http://repo1.maven.org/maven2/com/floragunn/search-guard-kibana-plugin/6.2.3-11/search-guard-kibana-plugin-6.2.3-11.zip
+# cd /root && wget http://repo1.maven.org/maven2/com/floragunn/search-guard-kibana-plugin/6.2.3-11/search-guard-kibana-plugin-6.2.3-11.zip
 
-cd /usr/share/kibana/bin && ./kibana-plugin install file:///root/search-guard-kibana-plugin-6.2.3-11.zip
+# cd /usr/share/kibana/bin && ./kibana-plugin install file:///root/search-guard-kibana-plugin-6.2.3-11.zip
+
+
+# sudo cp /home/vagrant/synced_folder/elasticsearch.yml /etc/elasticsearch
+# sudo cp /home/vagrant/synced_folder/keys/*.pem /etc/elasticsearch
+# sudo chown -R root:elasticsearch /etc/elasticsearch
+
+# sudo cp /home/vagrant/synced_folder/kibana.yml /etc/kibana
+# sudo chown -R root:root /etc/kibana/kibana.yml
+
+# sudo cp -r /home/vagrant/synced_folder/sgconfig /usr/share/elasticsearch/plugins/search-guard-6
+# sudo chown -R root:root /usr/share/elasticsearch/plugins/search-guard-6
+
+sudo cp /home/vagrant/synced_folder/keys/*.pem /etc/logstash
+sudo chown root:root /etc/logstash/*.pem
+sudo cp /home/vagrant/synced_folder/logstash.conf /etc/logstash/conf.d/
+
+# sudo systemctl restart kibana
+# sudo systemctl enable kibana
+
+# sudo systemctl restart elasticsearch
+# sudo systemctl enable elasticsearch
+
+# sudo systemctl restart logstash
+# sudo systemctl enable logstash
